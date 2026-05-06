a76080a1b7612691ed2196571d3857c320966436
rakshit-singhdev <rakshit.deligence@gmail.com>
2026-05-06 15:00:21 +0530

    hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter } from "react-router";
+import { createHashRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Login from './pages/Login.jsx';       
 import Dashboard from './pages/Dashboard.jsx';       
 import ResetPasswordPage from './pages/ForgotPassword.jsx';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Login/>,
