dba752999c5741909612c66d9542ecdd453c6de9
rakshit-singhdev <rakshit.deligence@gmail.com>
2026-05-06 14:54:12 +0530

    github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/sample-frontend/",
   server: {
     allowedHosts: [
       'primp-snowbird-geometry.ngrok-free.dev'
